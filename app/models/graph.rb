29738e1673ba6a15b517f1104174f9b861a9a814
Thiago Rose <thiago.senhorinha@gmail.com>
2015-04-03 20:50:41 -0300

    Implementando addVertex

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,2 +1,13 @@
 class Graph
+
+    def initialize
+        @vertices = Hash.new
+    end
+
+    def addVertex(vertex)
+        unless @vertices.member? vertex
+            @vertices[vertex] = Hash.new
+        end
+    end
+
 end
