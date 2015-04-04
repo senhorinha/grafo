f7d3501577ff1bd06a7369b292ce137850dc2a1e
Thiago Rose <thiago.senhorinha@gmail.com>
2015-04-03 22:40:33 -0300

    Refatorando e implementando connect

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -6,10 +6,24 @@ class Graph
         @vertices = Hash.new
     end
 
-    def addVertex(vertex)
-        unless @vertices.member? vertex
+    def has_vertex?(vertex)
+        @vertices.member? vertex
+    end
+
+    def add_vertex(vertex)
+        unless has_vertex? vertex
             @vertices[vertex] = Hash.new
         end
+    end
+
+    def connect(from_vertex, to_vertex)
+        if has_vertex?(from_vertex) && has_vertex?(to_vertex)
+            @vertices[from_vertex][to_vertex] = 1;
+        end
+    end
+
+    def connected?(from_vertex, to_vertex)
+        has_vertex?(from_vertex) && has_vertex?(to_vertex) && @vertices[from_vertex].member?(to_vertex)
     end
 
 end
--- test/models/graph_test.rb
+++ test/models/graph_test.rb
@@ -1,13 +1,38 @@
 require 'test_helper'
 
 class GraphTest < ActiveSupport::TestCase
+
     def setup
         @graph = Graph.new
+        @vertex_one = Vertex.new
+        @vertex_two = Vertex.new
+    end
+
+    def add_vertices
+        @graph.add_vertex @vertex_one
+        @graph.add_vertex @vertex_two
     end
 
     test "should add a new vertex" do
-        v = Vertex.new
-        @graph.addVertex(v);
-        assert_equal 1, @graph.vertices.length
+        @graph.add_vertex(@vertex_one)
+        assert_equal true, @graph.has_vertex?(@vertex_one)
     end
+
+    test "should add two new vertices" do
+        add_vertices
+        assert_equal true, @graph.has_vertex?(@vertex_one) && @graph.has_vertex?(@vertex_two)
+    end
+
+    test "should connect vertices" do
+        add_vertices
+        @graph.connect @vertex_one, @vertex_two
+        assert_equal true, @graph.connected?(@vertex_one, @vertex_two)
+    end
+
+    test "should not be connected" do
+        add_vertices
+        @graph.connect @vertex_one, @vertex_two
+        assert_equal false, @graph.connected?(@vertex_two, @vertex_one)
+    end
+
 end
